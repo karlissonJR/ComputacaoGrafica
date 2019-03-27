848b4f13325618cae0166374ee2e8e4a41c61dae
orodriguesjr <junior18playsz@hotmail.com>
2019-03-27 15:18:07 -0300

    textura e zoom

--- solar.js
+++ solar.js
@@ -5,8 +5,11 @@ var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
+var controls = new THREE.OrbitControls( camera, renderer.domElement );
+
 // Sun (spotlight).
-var sun = new THREE.SphereGeometry(3, 50, 50);
+//var textureLoader = new THREE.TextureLoader();
+var sun = new THREE.SphereBufferGeometry(3, 50, 50);
 var sunLight = new THREE.PointLight(0xffffff); 
 sunLight.position.set(0, 0, 0); 
 sunLight.castShadow = true; 
@@ -14,6 +17,7 @@ sunLight.shadowMapWidth = 1024;
 sunLight.shadowMapHeight = 1024; 
 sunLight.shadowCameraNear = 500; 
 sunLight.shadowCameraFar = 4000;
+//sunLight.add(new THREE.Mesh(sun, new THREE.MeshPhongMaterial({map: textureLoader.load('js/solar_system/textures/sun.jpg') })));
 sunLight.add(new THREE.Mesh(sun, new THREE.MeshBasicMaterial({ color: 0xffa500 })));
 sunLight.shadowCameraFov = 30;
 scene.add(sunLight);
@@ -24,57 +28,60 @@ light.position.set(0, 0, 50);
 scene.add( light );
 
 // mercury.
-var geometry = new THREE.SphereGeometry(0.3, 10, 10);
-var material = new THREE.MeshLambertMaterial({ color: 0xaaaaaa });
+//var texture = new THREE.ImageUtils.loadTexture('js/solar_system/textures/mercury1_baseColor.jpeg');
+var textureLoader = new THREE.TextureLoader();
+var geometry = new THREE.SphereBufferGeometry(0.3, 10, 10);
+var material = new THREE.MeshPhongMaterial({ map: textureLoader.load('js/solar_system/textures/mercury1_baseColor.jpeg')});
 var mercury = new THREE.Mesh( geometry, material );
 mercury.position.set(-5, 0, -5);
 scene.add(mercury);
 
 // Venus.
-var geometry = new THREE.SphereGeometry(0.4, 20, 20);
-var material = new THREE.MeshLambertMaterial( { color: 0xcc9900 } );
+var textureLoader = new THREE.TextureLoader();
+var geometry = new THREE.SphereBufferGeometry(0.4, 20, 20);
+var material = new THREE.MeshPhongMaterial( { map: textureLoader.load('js/solar_system/textures/venus1_baseColor.jpeg') } );
 var venus = new THREE.Mesh( geometry, material );
 venus.position.set(-7, 0, 7);
 scene.add(venus);
 
 // Earth.
-var geometry = new THREE.SphereGeometry(0.6, 20, 20);
-var material = new THREE.MeshLambertMaterial( { color: 0x0055ff } );
+var geometry = new THREE.SphereBufferGeometry(0.6, 20, 20);
+var material = new THREE.MeshPhongMaterial( { map: textureLoader.load('js/solar_system/textures/earth1_baseColor.jpeg') } );
 var earth = new THREE.Mesh( geometry, material );
 earth.position.set(20, 0, -20);
 scene.add(earth);
 
 // Mars.
-var geometry = new THREE.SphereGeometry(0.5, 20, 20);
-var material = new THREE.MeshLambertMaterial( { color: 0xff3355 } );
+var geometry = new THREE.SphereBufferGeometry(0.5, 20, 20);
+var material = new THREE.MeshPhongMaterial( { map: textureLoader.load('js/solar_system/textures/mars1_baseColor.jpeg') } );
 var mars = new THREE.Mesh( geometry, material );
 mars.position.set(10, 0, 10);
 scene.add(mars);
 
 // Jupiter.
-var geometry = new THREE.SphereGeometry(2, 20, 20);
-var material = new THREE.MeshLambertMaterial( { color: 0xbb6600 } );
+var geometry = new THREE.SphereBufferGeometry(2, 20, 20);
+var material = new THREE.MeshPhongMaterial( { map: textureLoader.load('js/solar_system/textures/jupiter1_baseColor.jpeg') } );
 var jupiter = new THREE.Mesh( geometry, material );
 jupiter.position.set(20, 0, -20);
 scene.add(jupiter);
 
 // Saturn.
-var geometry = new THREE.SphereGeometry(1.2, 20, 20);
-var material = new THREE.MeshLambertMaterial( { color: 0xdddd99 } );
+var geometry = new THREE.SphereBufferGeometry(1.2, 20, 20);
+var material = new THREE.MeshPhongMaterial( { map: textureLoader.load('js/solar_system/textures/saturn.jpg') } );
 var saturn = new THREE.Mesh( geometry, material );
 saturn.position.set(-10, 0, -20);
 scene.add(saturn);
 
 // Uranus.
-var geometry = new THREE.SphereGeometry(1, 20, 20);
-var material = new THREE.MeshLambertMaterial( { color: 0xaaffaa } );
+var geometry = new THREE.SphereBufferGeometry(1, 20, 20);
+var material = new THREE.MeshPhongMaterial( { map: textureLoader.load('js/solar_system/textures/uranus1_baseColor.jpeg') } );
 var uranus = new THREE.Mesh( geometry, material );
 uranus.position.set(20, 0, -20);
 scene.add(uranus);
 
 // Neptune.
-var geometry = new THREE.SphereGeometry(1, 20, 20);
-var material = new THREE.MeshLambertMaterial( { color: 0xaaccff } );
+var geometry = new THREE.SphereBufferGeometry(1, 20, 20);
+var material = new THREE.MeshPhongMaterial( { map: textureLoader.load('js/solar_system/textures/neptune1_baseColor.jpeg') } );
 var neptune = new THREE.Mesh( geometry, material );
 neptune.position.set(50, 0, -20);
 scene.add(neptune);
