5b59e7120d71012321930c15652569b193225143
orodriguesjr <junior18playsz@hotmail.com>
2019-04-03 19:59:23 -0300

    Final1

--- solar_system/main.js
+++ solar_system/main.js
@@ -1,16 +1,19 @@
 var pointLight, sun, moon, earth, earthOrbit, ring, controls, scene, camera, renderer, scene;
 var planetSegments = 50;
-var mercuryData = constructPlanetData(365.2564, 0.015, 20, "mercury", "img/mercury.jpeg", 0.5, planetSegments);
-var venusData = constructPlanetData(365.2564, 0.015, 25, "venus", "img/venus.jpeg", 0.8, planetSegments);
-var earthData = constructPlanetData(365.2564, 0.015, 30, "earth", "img/earth.jpg", 1, planetSegments);
+var mercuryData = constructPlanetData(87, 0.0004, 20, "mercury", "img/mercury.jpeg", 0.5, planetSegments);
+var venusData = constructPlanetData(225, .000099, 25, "venus", "img/venus.jpeg", 0.8, planetSegments);
+var earthData = constructPlanetData(365, 0.024, 30, "earth", "img/earth.jpg", 1, planetSegments);
 var moonData = constructPlanetData(29.5, 0, 2, "moon", "img/moon.jpg", 0.2, planetSegments);
-var marsData = constructPlanetData(365.2564, 0.015, 35, "mars", "img/mars.jpeg", 0.9, planetSegments);
-var jupyterData = constructPlanetData(365.2564, 0.015, 42, "jupyter", "img/jupyter.jpeg", 3, planetSegments);
-var saturnData = constructPlanetData(365.2564, 0.015, 50, "saturn", "img/saturn.jpg", 1.5, planetSegments);
-var uranusData = constructPlanetData(365.2564, 0.015, 55, "uranus", "img/uranus.jpeg", 1, planetSegments);
-var neptuneData = constructPlanetData(365.2564, 0.015, 60, "neptune", "img/neptune.jpeg", 1, planetSegments);
+var marsData = constructPlanetData(687, 0.025, 35, "mars", "img/mars.jpeg", 0.9, planetSegments);
+var jupyterData = constructPlanetData(4380, 0.0009, 42, "jupyter", "img/jupyter.jpeg", 3, planetSegments);
+var saturnData = constructPlanetData(10950, 0.01, 50, "saturn", "img/saturn.jpg", 1.5, planetSegments);
+var uranusData = constructPlanetData(60225, 0.017, 55, "uranus", "img/uranus.jpeg", 1, planetSegments);
+var neptuneData = constructPlanetData(59860, 0.016, 60, "neptune", "img/neptune.jpeg", 1, planetSegments);
+var planetOnFocus = null;
 
-var orbitData = {value: 200, runOrbit: true, runRotation: true};
+var planets = [];
+
+var orbitData = { value: 200, runOrbit: true, runRotation: true };
 var clock = new THREE.Clock();
 
 /**
@@ -48,7 +51,7 @@ function constructPlanetData(myOrbitRate, myRotationRate, myDistanceFromAxis, my
  */
 function getRing(size, innerDiameter, facets, myColor, name, distanceFromAxis) {
     var ring1Geometry = new THREE.RingGeometry(size, innerDiameter, facets);
-    var ring1Material = new THREE.MeshBasicMaterial({color: myColor, side: THREE.DoubleSide});
+    var ring1Material = new THREE.MeshBasicMaterial({ color: myColor, side: THREE.DoubleSide });
     var myRing = new THREE.Mesh(ring1Geometry, ring1Material);
     myRing.name = name;
     myRing.position.set(distanceFromAxis, 0, 0);
@@ -71,7 +74,7 @@ function getRing(size, innerDiameter, facets, myColor, name, distanceFromAxis) {
  */
 function getTube(size, innerDiameter, facets, myColor, name, distanceFromAxis) {
     var ringGeometry = new THREE.TorusGeometry(size, innerDiameter, facets, facets);
-    var ringMaterial = new THREE.MeshBasicMaterial({color: myColor, side: THREE.DoubleSide});
+    var ringMaterial = new THREE.MeshBasicMaterial({ color: myColor, side: THREE.DoubleSide });
     myRing = new THREE.Mesh(ringGeometry, ringMaterial);
     myRing.name = name;
     myRing.position.set(distanceFromAxis, 0, 0);
@@ -240,12 +243,12 @@ function movePlanet(myPlanet, myData, myTime, stopRotation) {
         myPlanet.rotation.y += myData.rotationRate;
     }
     if (orbitData.runOrbit) {
-        myPlanet.position.x = Math.cos(myTime 
-                * (1.0 / (myData.orbitRate * orbitData.value)) + 10.0) 
-                * myData.distanceFromAxis;
-        myPlanet.position.z = Math.sin(myTime 
-                * (1.0 / (myData.orbitRate * orbitData.value)) + 10.0) 
-                * myData.distanceFromAxis;
+        myPlanet.position.x = Math.cos(myTime
+            * (1.0 / (myData.orbitRate * orbitData.value)) + 10.0)
+            * myData.distanceFromAxis;
+        myPlanet.position.z = Math.sin(myTime
+            * (1.0 / (myData.orbitRate * orbitData.value)) + 10.0)
+            * myData.distanceFromAxis;
     }
 }
 
@@ -304,6 +307,10 @@ function update(renderer, scene, camera, controls) {
     movePlanet(ring, saturnData, time, true);
     moveMoon(moon, earth, moonData, time);
 
+    if (planetOnFocus !== null) {
+        //camera.position.set(planetOnFocus.position.x, planetOnFocus.position.y, planetOnFocus.position.z);
+        controls.target.set(planetOnFocus.position.x, planetOnFocus.position.y, planetOnFocus.position.z);
+    }
 
     renderer.render(scene, camera);
     requestAnimationFrame(function () {
@@ -318,14 +325,14 @@ function update(renderer, scene, camera, controls) {
 function init() {
     // Create the camera that allows us to view into the scene.
     camera = new THREE.PerspectiveCamera(
-            45, // field of view
-            window.innerWidth / window.innerHeight, // aspect ratio
-            1, // near clipping plane
-            1000 // far clipping plane
-            );
-    camera.position.z = 30;
+        45, // field of view
+        window.innerWidth / window.innerHeight, // aspect ratio
+        1, // near clipping plane
+        1000 // far clipping plane
+    );
+    camera.position.z = 100;
     camera.position.x = -30;
-    camera.position.y = 30;
+    camera.position.y = 40;
     camera.lookAt(new THREE.Vector3(0, 0, 0));
 
     // Create the scene that holds all of the visible objects.
@@ -370,16 +377,17 @@ function init() {
 
     // Create the glow of the sun.
     var spriteMaterial = new THREE.SpriteMaterial(
-            {
-                map: new THREE.ImageUtils.loadTexture("img/glow.png")
-                , useScreenCoordinates: false
-                , color: 0xffffee
-                , transparent: false
-                , blending: THREE.AdditiveBlending
-            });
+        {
+            map: new THREE.ImageUtils.loadTexture("img/glow.png")
+            , useScreenCoordinates: false
+            , color: 0xffffee
+            , transparent: false
+            , blending: THREE.AdditiveBlending
+        });
     var sprite = new THREE.Sprite(spriteMaterial);
     sprite.scale.set(70, 70, 1.0);
     sun.add(sprite); // This centers the glow at the sun.
+    sun.name = "sun";
 
     // Create the planets
     mercury = loadTexturedPlanet(mercuryData, mercuryData.distanceFromAxis, 0, 0);
@@ -393,21 +401,113 @@ function init() {
     uranus = loadTexturedPlanet(uranusData, uranusData.distanceFromAxis, 0, 0);
     neptune = loadTexturedPlanet(neptuneData, neptuneData.distanceFromAxis, 0, 0);
 
+    planets.push(mercury, venus, mars, earth, moon, jupyter, saturn, uranus, neptune, sun);
+
+    info = document.getElementById('contentTitle');
+    subtitle = document.getElementById('subtitle');
+    description = document.getElementById('description')
+
     // Create the visible orbit that the Earth uses.
     createVisibleOrbits();
 
-    // Create the GUI that displays controls.
-    var gui = new dat.GUI();
-    var folder1 = gui.addFolder('light');
-    folder1.add(pointLight, 'intensity', 0, 10);
-    var folder2 = gui.addFolder('speed');
-    folder2.add(orbitData, 'value', 0, 500);
-    folder2.add(orbitData, 'runOrbit', 0, 1);
-    folder2.add(orbitData, 'runRotation', 0, 1);
+    
 
     // Start the animation.
     update(renderer, scene, camera, controls);
 }
 
+function setFromCamera(raycaster, coords, origin) {
+    raycaster.ray.origin.copy(camera.position);
+    raycaster.ray.direction.set(coords.x, coords.y, 0.5).unproject(camera).sub(camera.position).normalize();
+}
+
+function hideWelcome() {
+    TweenMax.to($('#welcome'), 0.5, {
+        css: {
+            opacity: 0
+        },
+        ease: Quad.easeInOut
+    });
+    TweenMax.to($('#welcome'), 0.5, {
+        css: {
+            display: 'none'
+        },
+        delay: 1,
+        ease: Quad.easeInOut
+    });
+}
+
+function onMouseDown(event) {
+    raycaster = new THREE.Raycaster();
+    mouse = new THREE.Vector2();
+    mouse.x = (event.clientX / renderer.domElement.width) * 2 - 1;
+    mouse.y = -(event.clientY / renderer.domElement.height) * 2 + 1;
+    setFromCamera(raycaster, mouse, camera);
+    var intersects = raycaster.intersectObjects(planets);
+    if (intersects.length > 0) {
+        hideWelcome();
+        TweenMax.from($('#content'), 0.5, {
+            css: {
+                left: '-500px'
+            },
+            delay: 0.5,
+            ease: Quad.easeInOut
+        });
+        planetOnFocus = intersects[0].object;
+        switch (intersects[0].object.name) {
+            case "mercury":
+                info.innerHTML = " <span>Mercúrio</span>";
+                description.innerHTML = "O menor e mais interno planeta do Sistema Solar, orbitando o Sol a cada 87,969 dias terrestres. A sua órbita tem a maior excentricidade e o seu eixo apresenta a menor inclinação em relação ao plano da órbita dentre todos os planetas do Sistema Solar.";
+                break;
+            case "venus":
+                info.innerHTML = " <span>Vênus</span>";
+                description.innerHTML = "O segundo planeta do Sistema Solar em ordem de distância a partir do Sol, orbitando-o a cada 224,7 dias. Recebeu seu nome em homenagem à deusa romana do amor e da beleza Vénus, equivalente a Afrodite. Depois da Lua, é o objeto mais brilhante do céu noturno, atingindo uma magnitude aparente de -4,6, o suficiente para produzir sombras.";
+                break;
+            case "mars":
+                info.innerHTML = " <span>Marte</span>";
+                description.innerHTML = "O quarto planeta a partir do Sol, o segundo menor do Sistema Solar. Batizado em homenagem ao deus romano da guerra, muitas vezes é descrito como o 'Planeta Vermelho', porque o óxido de ferro predominante em sua superfície lhe dá uma aparência avermelhada.";
+                break;
+            case "earth":
+                info.innerHTML = " <span>Terra</span>";
+                description.innerHTML = "O terceiro planeta mais próximo do Sol, o mais denso e o quinto maior dos oito planetas do Sistema Solar. É também o maior dos quatro planetas telúricos. É por vezes designada como Mundo ou Planeta Azul. Lar de milhões de espécies de seres vivos, incluindo os humanos, a Terra é o único corpo celeste onde é conhecida a existência de vida.";
+                break;
+            case "moon":
+                info.innerHTML = " <span>Lua</span>";
+                description.innerHTML = "A Lua é o único satélite natural da Terra e o quinto maior do Sistema Solar. É o maior satélite natural de um planeta no sistema solar em relação ao tamanho do seu corpo primário, tendo 27% do diâmetro e 60% da densidade da Terra, o que representa 1⁄81 da sua massa.";
+                break;
+            case "jupyter":
+                info.innerHTML = " <span>Júpiter</span>";
+                description.innerHTML = "O maior planeta do Sistema Solar, tanto em diâmetro quanto em massa, e é o quinto mais próximo do Sol. Possui menos de um milésimo da massa solar, contudo tem 2,5 vezes a massa de todos os outros planetas em conjunto.";
+                break;
+            case "saturn":
+                info.innerHTML = " <span>Saturno</span>";
+                description.innerHTML = "Sexto planeta a partir do Sol e o segundo maior do Sistema Solar atrás de Júpiter. Pertencente ao grupo dos gigantes gasosos, possui cerca de 95 massas terrestres e orbita a uma distância média de 9,5 unidades astronômicas.";
+                break;
+            case "uranus":
+                info.innerHTML = " <span>Urano</span>";
+                description.innerHTML = "O sétimo planeta a partir do Sol, o terceiro maior e o quarto mais massivo dos oito planetas do Sistema Solar. Foi nomeado em homenagem ao deus grego do céu, Urano, o pai de Cronos (Saturno) e o avô de Zeus (Júpiter).";
+                break;
+            case "neptune":
+                info.innerHTML = " <span>Netuno</span>";
+                description.innerHTML = "O oitavo planeta do Sistema Solar, o último a partir do Sol desde a reclassificação de Plutão para a categoria de planeta anão, em 2006. Pertencente ao grupo dos gigantes gasosos, possui um tamanho ligeiramente menor que o de Urano, mas maior massa, equivalente a 17 massas terrestres. Netuno orbita o Sol a uma distância média de 30,1 unidades astronômicas.";
+                break;
+            case "sun":
+                info.innerHTML = " <span>Sol</span>";
+                description.innerHTML = "A estrela central do Sistema Solar. Todos os outros corpos do Sistema Solar, como planetas, planetas anões, asteroides, cometas e poeira, bem como todos os satélites associados a estes corpos, giram ao seu redor. Responsável por 99,86% da massa do Sistema Solar, o Sol possui uma massa 332 900 vezes maior que a da Terra, e um volume 1 300 000 vezes maior que o do nosso planeta. A distância da Terra ao Sol é cerca de 150 milhões de quilômetros ou 1 unidade astronômica (UA).";
+                break;
+        }
+    }
+}
+document.addEventListener('mousedown', onMouseDown, false);
+
 // Start everything.
 init();
+
+$(window).on('load', function () {
+    TweenMax.to($('#welcome'), 1, {
+        css: {
+            opacity: 1
+        },
+        ease: Quad.easeInOut,
+    });
+});
